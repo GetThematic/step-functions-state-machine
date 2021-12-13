da6d4bcfb80f24bae7d323465da62cb557f2a709
Nathan Holmberg <nathan@getthematic.com>
2021-12-14 12:17:55 +1300

    Attempt to fix data install issue

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ setup(
     author_email="contact@getthematic.com",
     url="http://getthematic.com/",
     description="",
+
+    package_data={
+        'step_functions_local': ['step_functions_local/data/*'],
+    },
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     install_requires=["jsonschema>=4.0.0", "jsonpath-ng==1.5.3"],
