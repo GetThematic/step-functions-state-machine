e258a27df36c3a8453ddc2cf1f1f956829ef6de1
Nathan Holmberg <nathan@getthematic.com>
2026-03-02 10:47:12 +1300

    PORTAL-7499: migrate to uv

--- setup.sh
+++ setup.sh
@@ -2,9 +2,4 @@
 set -x
 set -e
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-virtualenv --python python3.12 env
-source env/bin/activate
-
-pip install --upgrade pip
-pip install -r requirements.txt
-
+UV_PROJECT_ENVIRONMENT=env uv sync --group dev
