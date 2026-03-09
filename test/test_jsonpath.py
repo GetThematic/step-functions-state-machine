452e2c36a9a3484bd2a7bed38c643e306890e6b0
Nathan Holmberg <nathan@getthematic.com>
2026-03-10 09:29:06 +1300

    PORTAL-7449: update jsonschema package

--- test/test_jsonpath.py
+++ test/test_jsonpath.py
@@ -13,7 +13,7 @@ def test_jsonpath():
     }
     assert jsonpath.get_json_value(data, "$.foo") == 123
     assert jsonpath.get_json_value(data, "$.bar") == ["a", "b", "c"]
-    assert jsonpath.get_json_value(data, "$.car.cdr") == True
+    assert jsonpath.get_json_value(data, "$.car.cdr") is True
 
     # should assign the value to the specified path)
     data = {
